bb966d3f16df8f4172f40c364e378f39ce4fb31e
Your Name <you@example.com>
2017-10-04 18:38:36 +0800

    Add searchBar to my app. There is an problem that the when I tap a cell, it goes to a wrong place. I think that index is the reason.

--- ChinaWeather/ProvinceNameViewController.swift
+++ ChinaWeather/ProvinceNameViewController.swift
@@ -15,24 +15,45 @@ class provinceNameViewController: UITableViewController {
     var data: NSDictionary?
     var chinaAddress: ChinaAddress?
     
+    var originalArray: [String] = []
+    var searchingDataArray: [String] = []
+    var search = false //This for search is on or not that identifier
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         data = NSDictionary(contentsOf: Bundle.main.url(forResource: "address", withExtension: "plist")!)
         chinaAddress = ChinaAddress(dictionary: data as! [String : AnyObject])
+        
+        for item in (chinaAddress?.address)! {
+            let province = item["name"] as! String
+            originalArray.append(province)
+        }
+        //self.hideKeyboard()
     }
     
     // UITableViewDataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //print("cell number is \((chinaAddress?.address.count)!)")
-        return (chinaAddress?.address.count)!
+        //return (chinaAddress?.address.count)!
+        if search == true {
+            return searchingDataArray.count
+        } else {
+            return originalArray.count
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "provinceCell", for: indexPath)
-        let addressCell = chinaAddress?.address[indexPath.row]
-        cell.textLabel?.text = addressCell?["name"] as? String
+        //let addressCell = chinaAddress?.address[indexPath.row]
+        if search == true {
+            cell.textLabel?.text = searchingDataArray[indexPath.row]
+        } else {
+            cell.textLabel?.text = originalArray[indexPath.row]
+        }
         return cell
-        
     }
     
     // UITableViewDelegate
@@ -43,7 +64,38 @@ class provinceNameViewController: UITableViewController {
         cityNameViewController.province = province
         
         self.navigationController?.pushViewController(cityNameViewController, animated: true)
+    }
+}
+
+extension provinceNameViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
+        searchingDataArray = originalArray.filter({ (text) -> Bool in
+            let tmp: NSString = text as NSString
+            let range = tmp.range(of: searchText, options: NSString.CompareOptions.caseInsensitive)
+            return range.location != NSNotFound
+        })
+        if searchingDataArray.count == 0 {
+            search = false
+        } else {
+            search = true
+        }
+        self.tableView.reloadData()
     }
 
+}
+
+extension provinceNameViewController {
+    func hideKeyboard() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(provinceNameViewController.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
