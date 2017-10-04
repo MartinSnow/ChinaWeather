39d088ba6540052169529f5b0dd6bd9f8f0e56b1
Your Name <you@example.com>
2017-10-04 13:48:39 +0800

    Display all cities' coordinator

--- ChinaWeather/CityNameViewController.swift
+++ ChinaWeather/CityNameViewController.swift
@@ -15,6 +15,7 @@ class cityNameViewController: UIViewController, UITableViewDataSource, UITableVi
     var weatherList: [[String:AnyObject]]?
     var cityName: String?
     var coordinator: [Float]?
+    let specialCitites = ["北京","天津","上海","重庆","香港特别行政区","澳门特别行政区","台湾"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,33 +50,61 @@ extension cityNameViewController {
     
     func getCoordinator(provinceName: String, cityName: String){
         
-        //if provinceName != "北京"
-        let path = Bundle.main.path(forResource: provinceName, ofType: "json")
-        let pathURL = URL(fileURLWithPath: path!)
-        let data = NSData(contentsOf: pathURL)
-        
-        WeatherClient.sharedInstance.convertDataWithCompletionHandler(data as! Data){(parsedResult, error) in
-        
-            if error != nil {
-                print("Fail to get parsedResult")
-                return
-            }
+        if !specialCitites.contains(provinceName) {
+            let path = Bundle.main.path(forResource: provinceName, ofType: "json")
+            let pathURL = URL(fileURLWithPath: path!)
+            let data = NSData(contentsOf: pathURL)
             
-            DispatchQueue.main.async{
-                if let features = parsedResult?["features"] as? [[String: AnyObject]] {
-                    for feature in features {
-                        if let properties = feature["properties"] as? [String: AnyObject] {
-                            if let name = properties["name"] as? String {
-                                if name == cityName {
-                                    self.coordinator = properties["cp"] as? [Float]
-                                    print("\(cityName), \(self.coordinator)")
+            WeatherClient.sharedInstance.convertDataWithCompletionHandler(data as! Data){(parsedResult, error) in
+                
+                if error != nil {
+                    print("Fail to get parsedResult")
+                    return
+                }
+                
+                DispatchQueue.main.async{
+                    if let features = parsedResult?["features"] as? [[String: AnyObject]] {
+                        for feature in features {
+                            if let properties = feature["properties"] as? [String: AnyObject] {
+                                if let name = properties["name"] as? String {
+                                    if name == cityName {
+                                        self.coordinator = properties["cp"] as? [Float]
+                                        print("\(cityName), \(self.coordinator)")
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
+        } else {
+            switch provinceName {
+            case "北京":
+                self.coordinator = [117.0923,40.5121]
+                print("\(provinceName), \(self.coordinator)")
+            case "天津":
+                self.coordinator = [117.4672,40.004]
+                print("\(provinceName), \(self.coordinator)")
+            case "上海":
+                self.coordinator = [121.4333,31.1607]
+                print("\(provinceName), \(self.coordinator)")
+            case "重庆":
+                self.coordinator = [108.8196,28.8666]
+                print("\(provinceName), \(self.coordinator)")
+            case "香港特别行政区":
+                self.coordinator = [114.1657, 22.2793]
+                print("\(provinceName), \(self.coordinator)")
+            case "澳门特别行政区":
+                self.coordinator = [113.5586, 22.1630]
+                print("\(provinceName), \(self.coordinator)")
+            case "台湾":
+                self.coordinator = [121.0193, 23.4366]
+                print("\(provinceName), \(self.coordinator)")
+            default:
+                return
+            }
         }
+        
     }
 
 }
